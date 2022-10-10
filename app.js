e2e2062d536202154c4314b0e2ed222b19a26d0a
Ben Sales <adebsa2401@gmail.com>
2022-10-10 23:18:04 +0100

    Fix cors issues

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const orderRoutes = require('./routes/order');
+const express = require("express");
+const orderRoutes = require("./routes/order");
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-app.use('/orders', orderRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
