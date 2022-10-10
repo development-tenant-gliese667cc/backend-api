f89031d42ab2db204000b9c3ad091d07f748849b
Ben Sales <adebsa2401@gmail.com>
2022-10-10 18:23:31 +0100

    Fix db loading error

--- databases/db.js
+++ databases/db.js
@@ -1,17 +1,16 @@
-const firebase = require('./firebase');
-const postgres = require('./postgres');
-const all = require('./all');
-
-require('dotenv').config()
+require('dotenv').config();
 
 switch (process.env.DATA_PROVIDER) {
     case 'postgres':
+        const postgres = require('./postgres');
         module.exports = postgres;
         break;
     case 'firebase':
+        const firebase = require('./firebase');
         module.exports = firebase;
         break;
     case 'all':
+        const all = require('./all');
         module.exports = all;
         break;
     default:
